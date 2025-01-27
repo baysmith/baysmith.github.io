edbda8c19daf6793a697a8f834c5fd28932f2cfb
Bradley Smith <bradley@baysmith.com>
2025-01-26 21:50:54 -0800

    updated dev/spring

---
+++ dev/spring/sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'greed-score';
+var filesToCache = [
+  './',
+  './index.html',
+  './spring.js',
+  './spring_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
